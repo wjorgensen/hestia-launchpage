e6cb73edf063879bbaa1d24c3f2358ea62b2f646
WesJorgensen <wjorgensen29@gmail.com>
2022-09-29 17:39:25 -0700

    Delete hestia_launch_page directory

--- hestia_launch_page/main.js
+++
@@ -1,7 +0,0 @@
-
-
-function onClick(){
-    const submitbutton = document.getElementById("submit");
-    submitbutton.textContent="Thank you for\r\nsigning up";
-    submitbutton.innerHTML = submitbutton.innerHTML.replace(/\n\r?/g, '<br />');
-}
